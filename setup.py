45bdada2286866ee79410dc22472d781e0a3f063
Nico Mandel <mandel.nicolas@gmail.com>
2024-02-28 15:33:49 +0100

    added base files for initialising the model

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-        name='csupl',
-        version='0.1.0',
+        name='alg',
+        version='0.0.1',
         packages=find_packages(include=['src']),
         package_dir={'':'src'}
         )
