4913259b7341f84bf1420c3ca703a353a35c5cfb
Nico Mandel <mandel.nicolas@gmail.com>
2024-02-28 15:39:58 +0100

    added basic tutorials for ResNet18

---
+++ scripts/inference_model.py
@@ -0,0 +1,6 @@
+# https://github.com/Stevellen/ResNet-Lightning/blob/master/resnet_classifier.py
+# lightning Changelog: https://lightning.ai/docs/pytorch/stable/upgrade/from_1_7.html
+# tutorial: https://lightning.ai/docs/pytorch/stable/notebooks/course_UvA-DL/04-inception-resnet-densenet.html
+
+if __name__=="__main__":
+    pass
