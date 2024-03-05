fc6d05688782e78271fd79b8447183165a83db7b
Nico Mandel <mandel.nicolas@gmail.com>
2024-03-05 12:18:43 +0100

    adding rough default structure for model

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,34 +1,129 @@
 # https://github.com/Stevellen/ResNet-Lightning/blob/master/resnet_classifier.py
 # lightning Changelog: https://lightning.ai/docs/pytorch/stable/upgrade/from_1_7.html
 # tutorial: https://lightning.ai/docs/pytorch/stable/notebooks/course_UvA-DL/04-inception-resnet-densenet.html
-import os.path
-import numpy as np
-import matplotlib.pyplot as plt
+import warnings
+from argparse import ArgumentParser
+from pathlib import Path
 
-from alg.dataloader import ALGDataset
-from alg.utils import load_config
+warnings.filterwarnings("ignore")
 
-if __name__=="__main__":
-    basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+import pytorch_lightning as pl
+import torch
+from alg.model import ResNetClassifier
 
-    datadir = os.path.join(basedir, 'data')
-    alg_ds = ALGDataset(datadir)
+def parse_args():
+    parser = ArgumentParser()
+    # Required arguments
+    parser.add_argument(
+        "model",
+        help="""Choose one of the predefined ResNet models provided by torchvision. e.g. 50""",
+        type=int,
+    )
+    parser.add_argument(
+        "num_classes", help="""Number of classes to be learned.""", type=int
+    )
+    parser.add_argument("num_epochs", help="""Number of Epochs to Run.""", type=int)
+    parser.add_argument(
+        "train_set", help="""Path to training data folder.""", type=Path
+    )
+    parser.add_argument("val_set", help="""Path to validation set folder.""", type=Path)
+    # Optional arguments
+    parser.add_argument(
+        "-amp",
+        "--mixed_precision",
+        help="""Use mixed precision during training. Defaults to False.""",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-ts", "--test_set", help="""Optional test set path.""", type=Path
+    )
+    parser.add_argument(
+        "-o",
+        "--optimizer",
+        help="""PyTorch optimizer to use. Defaults to adam.""",
+        default="adam",
+    )
+    parser.add_argument(
+        "-lr",
+        "--learning_rate",
+        help="Adjust learning rate of optimizer.",
+        type=float,
+        default=1e-3,
+    )
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        help="""Manually determine batch size. Defaults to 16.""",
+        type=int,
+        default=16,
+    )
+    parser.add_argument(
+        "-tr",
+        "--transfer",
+        help="""Determine whether to use pretrained model or train from scratch. Defaults to True.""",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-to",
+        "--tune_fc_only",
+        help="Tune only the final, fully connected layers.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-s", "--save_path", help="""Path to save model trained model checkpoint."""
+    )
+    parser.add_argument(
+        "-g", "--gpus", help="""Enables GPU acceleration.""", type=int, default=None
+    )
+    return parser.parse_args()
 
-    confdir = os.path.join(basedir, 'config')
-    conff = os.path.join(confdir, 'config.yml')
-    cfg = load_config(conff)
 
-    print(len(alg_ds))
-    for j in range(10000):
-        fig,axs = plt.subplots(1,2, figsize=(16,9))
-        i = np.random.randint(len(alg_ds))
-        name = alg_ds.img_list[i]
-        img, mask = alg_ds[i]
-        axs[0].imshow(img)
-        axs[0].axis('off')
-        axs[1].imshow(mask)
-        axs[1].axis('off')
-        plt.suptitle("Sample {}, img: {}".format(i, name))
-        plt.show()
+if __name__ == "__main__":
+    args = parse_args()
+
+    # # Instantiate Model
+    model = ResNetClassifier(
+        num_classes=args.num_classes,
+        resnet_version=args.model,
+        train_path=args.train_set,
+        val_path=args.val_set,
+        test_path=args.test_set,
+        optimizer=args.optimizer,
+        lr=args.learning_rate,
+        batch_size=args.batch_size,
+        transfer=args.transfer,
+        tune_fc_only=args.tune_fc_only,
+    )
+
+    save_path = args.save_path if args.save_path is not None else "./models"
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=save_path,
+        filename="resnet-model-{epoch}-{val_loss:.2f}-{val_acc:0.2f}",
+        monitor="val_loss",
+        save_top_k=3,
+        mode="min",
+        save_last=True,
+    )
+
+    stopping_callback = pl.callbacks.EarlyStopping()
+
+    # Instantiate lightning trainer and train model
+    trainer_args = {
+        "accelerator": "gpu" if args.gpus else None,
+        "devices": [1],
+        "strategy": "dp" if args.gpus > 1 else None,
+        "max_epochs": args.num_epochs,
+        "callbacks": [checkpoint_callback],
+        "precision": 16 if args.mixed_precision else 32,
+    }
+    trainer = pl.Trainer(**trainer_args)
+
+    trainer.fit(model)
+
+    if args.test_set:
+        trainer.test(model)
+    # Save trained model weights
+    torch.save(trainer.model.resnet_model.state_dict(), save_path + "/trained_model.pt")
+
 
         
