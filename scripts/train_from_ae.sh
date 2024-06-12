52357a594b3f4439c14d15a829251f5e7507c3a2
Nico Mandel <mandel.nicolas@gmail.com>
2024-06-12 15:26:12 +0200

    changed volume for ae training to 0.01

--- scripts/train_from_ae.sh
+++ scripts/train_from_ae.sh
@@ -6,11 +6,11 @@ for ds in "${dss[@]}"
 do  
     for i in "${resnets[@]}"
     do
-        python "scripts/train_from_ae.py" 18 1 200 $ds $i  --limit 0.1
+        python "scripts/train_from_ae.py" 18 1 200 $ds $i  --limit 0.01
     done 
 done
 
 for ds in "${dss[@]}"
 do  
-    python "scripts/train_from_ae.py" 34 1 200 $ds "ae/RN34-32.ckpt" --limit 0.1
+    python "scripts/train_from_ae.py" 34 1 200 $ds "ae/RN34-32.ckpt" --limit 0.01
 done
