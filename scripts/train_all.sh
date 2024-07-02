914b93ff80e994b21112386b4a9be042204cebed
Nico Mandel <mandel.nicolas@gmail.com>
2024-07-02 12:22:48 +0200

    added outside scripts for training full loops

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,25 +1,53 @@
 #!/usr/bin/bash
 clean_datadirs() {
-    rm /home/mandel/data_ssd/ALG/labeled/images/*.tif
-    rm /home/mandel/data_ssd/ALG/labeled/labeled/*.tif
-    rm /home/mandel/data_ssd/ALG/raw/images/*.png
-    echo "cleaned /home/mandel/data_ssd/ALG/"
+    rm /home/mandel/data_ssd/v2/data/labeled/images/*.tif
+    rm /home/mandel/data_ssd/v2/data/labeled/labeled/*.tif
+    rm /home/mandel/data_ssd/v2/data/raw/images/*.png
+    echo "cleaned /home/mandel/data_ssd/v2/data/"
 }
+
+# number setup
+n_labeled=100
+n_unlabeled=20
+epochs_labeled=5
+epochs_unlabeled=5
+name="eccv_v2"
 
 # Cleaning before a new run!
 clean_datadirs
 
-# run subensemble
-python scripts/subensemble_pipeline.py  --sample 1
-echo "completed running subensemble with sampling"
+# run subensemble with normal autoencoder
+python scripts/subensemble_pipeline.py $name --autoenc --sample --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
+echo "completed running subensemble with normal autoencoder and ensemble sampling"
 clean_datadirs
-python scripts/subensemble_pipeline.py --sample 0
-echo "completed running subensemble without sampling"
+python scripts/subensemble_pipeline.py $name --autoenc --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
+echo "completed running subensemble with normal autoencoder and without sampling"
+clean_datadirs
+
+# run subensemble with denoising autoencoder
+python scripts/subensemble_pipeline.py $name --autoenc --denoising --sample --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
+echo "completed running subensemble with denoising autoencoder and sampling"
+clean_datadirs
+python scripts/subensemble_pipeline.py $name --autoenc --denoising --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
+echo "completed running subensemble with denoising autoencoder without sampling"
+clean_datadirs
+
+# run subensemble without autoencoder training at all
+python scripts/subensemble_pipeline.py $name --sample --n_labeled $n_labeled --epochs_labeled $epochs_labeled 
+echo "completed running subensemble without autoencoder with sampling"
+clean_datadirs
+python scripts/subensemble_pipeline.py $name --n_labeled $n_labeled --epochs_labeled $epochs_labeled
+echo "completed running subensemble without autoencoder and without sampling"
 clean_datadirs
 
 # run baseline - autoencoder
-python scripts/baseline_ae.py
+python scripts/baseline_ae.py $name --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
 clean_datadirs
-# baseline - no autoencoder
-python scripts/baseline_resnet.py
+
+# run baseline - denoising autoencoder
+python scripts/baseline_ae.py $name --denoising --n_labeled $n_labeled --epochs_labeled $epochs_labeled --epochs_unlabeled $epochs_unlabeled --n_unlabeled $n_unlabeled
+clean_datadirs
+
+# baseline - no autoencoder just a resnet
+python scripts/baseline_resnet.py $name --n_labeled $n_labeled --epochs_labeled $epochs_labeled
 
---
+++ scripts/train_labeled_only.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+clean_datadirs() {
+    rm /home/mandel/data_ssd/no_ae/data/labeled/images/*.tif
+    rm /home/mandel/data_ssd/no_ae/data/labeled/labeled/*.tif
+    echo "cleaned /home/mandel/data_ssd/no_ae/data/"
+}
+
+# number setup
+n_labeled=100
+epochs_labeled=5
+name="eccv_no_ae"
+
+# Cleaning before a new run!
+clean_datadirs
+
+# run subensemble without autoencoder training at all
+python scripts/subensemble_pipeline.py $name --sample --n_labeled $n_labeled --epochs_labeled $epochs_labeled
+echo "completed running subensemble without autoencoder with sampling"
+clean_datadirs
+python scripts/subensemble_pipeline.py $name --n_labeled $n_labeled --epochs_labeled $epochs_labeled
+echo "completed running subensemble without autoencoder and without sampling"
+clean_datadirs
+
+# baseline - no autoencoder just a resnet
+python scripts/baseline_resnet.py $name --n_labeled $n_labeled --epochs_labeled $epochs_labeled
+
